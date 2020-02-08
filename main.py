16d90ef65aeeef2609f914fd91049f47bd3f6e77
AhmedAbo6 <ahmed.abo.sita66@gmail.com>
2020-02-08 19:34:55 +0200

    Made the entering frame

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ from tkinter import *
 root = Tk()
 root.title("El7ara Members Attendance")
 
+entering_frame = LabelFrame(root,text="Entering", padx=20, pady=20)
+entering_frame.pack(padx=30, pady=30)
 
+l1 = Label(entering_frame, text="Name or Number: ")
+e1 = Entry(entering_frame)
 
-
+l1.grid(column=0, row=0)
+e1.grid(column=1, row=0)
 
 
 
